905b905dbecccf602ba6a7f03c0e30fc06e735e5
keti <qeti.tsiklauri320@ens.tsu.edu.ge>
2025-11-05 19:57:52 +0400

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,29 +5,34 @@ import "./Navbar.css";
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
 
+  // Function to close menu when a link is clicked
+  const handleLinkClick = () => {
+    setShowMenu(false);
+  };
+
   return (
     <nav className="navbar">
-      <div className="logo" style={{ color: " #e0d4f7", fontWeight: "700" }}>
+      <div className="logo" style={{ color: "#e0d4f7", fontWeight: "700" }}>
         TechStore
       </div>
 
       <div className={`nav-links ${showMenu ? "show" : ""}`}>
-        <NavLink to="/" className="link">
+        <NavLink to="/" className="link" onClick={handleLinkClick}>
           Main
         </NavLink>
-        <NavLink to="/mobile-phones" className="link">
+        <NavLink to="/mobile-phones" className="link" onClick={handleLinkClick}>
           Mobile Phones
         </NavLink>
-        <NavLink to="/laptops" className="link">
+        <NavLink to="/laptops" className="link" onClick={handleLinkClick}>
           Laptops
         </NavLink>
-        <NavLink to="/computers" className="link">
+        <NavLink to="/computers" className="link" onClick={handleLinkClick}>
           Computers
         </NavLink>
-        <NavLink to="/accessories" className="link">
+        <NavLink to="/accessories" className="link" onClick={handleLinkClick}>
           Accessories
         </NavLink>
-        <NavLink to="/cart" className="link">
+        <NavLink to="/cart" className="link" onClick={handleLinkClick}>
           Cart
         </NavLink>
       </div>
